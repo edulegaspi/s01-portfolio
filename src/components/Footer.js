e889a99771a1703096ff5455e7d209dc9518d982
edulegaspi <edu.torres.legaspi@gmail.com>
2021-09-20 21:25:16 +0800

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppFooter() {
+  return (
+    <div class="d-flex justify-content-center bg-dark my-5 py-5">
+      This is the footer
+    </div>
+  )
+}

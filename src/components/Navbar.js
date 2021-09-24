b1e938a89116b61dbc0506344e58de798b849e50
edulegaspi <edu.torres.legaspi@gmail.com>
2021-09-24 18:15:12 +0800

    added news to navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,7 @@ export default function AppNavbar() {
                 <Nav.Link href="#link">Link</Nav.Link>
                 <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#contact">Contact Us</Nav.Link>
+                <Nav.Link href="#news">News</Nav.Link>
             </Nav>
         </Navbar>
     )
